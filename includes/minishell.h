f646f07f8f31db4250422301658e382ba91ec280
Maria Barreiro <mariaeobarreiro@gmail.com>
2025-11-05 14:21:05 +0000

    to do: finish studying the structs and focus on the main loop

--- includes/minishell.h
+++ includes/minishell.h
@@ -22,41 +22,64 @@
 
 //Structs
 
+
+/*
+	Lexical layer.
+	The enum classifies the kinds of tokens possible
+		that the lexer can find.
+*/
 typedef enum e_token_type
 {
-    T_WORD,
-    T_PIPE,
-    T_REDIR_IN, 
-    T_REDIR_OUT,
-    T_REDIR_APPEND,
-    T_REDIR_FILE,
-    T_HEREDOC,
-//    T_HEREDOC_DELIMITER,
+    T_WORD,					//normal word like "ls"
+    T_PIPE,					// | 
+    T_REDIR_IN,				// <
+    T_REDIR_OUT,			// >
+    T_REDIR_APPEND,			// >>
+    T_HEREDOC,				// <<
 } t_token_type;
+
+/*
+	Store each token in a linked list node.
+*/
 
 typedef struct s_token
 {
-    t_token_type    type;
     char            *word;
+    t_token_type    type;
     struct s_token  *next;
-    struct s_token  *prev;
 } t_token;
 
 typedef struct  s_shell
 {
-    char    *input;
-    t_token *tokens;
+    char    **args;
+	char	**env;
+	int		exit_status;
 } t_shell;
+
+typedef struct s_tokenizer
+{
+	int					redir_in;
+	int					redir_out;
+	int					redir_append;
+	int					heredoc;
+	int					heredoc_fd;
+	char				**limits;
+	char				**args;
+	char				**input;
+	char				**output;
+	struct s_tokenizer	*next;
+} t_tokenizer;
 
 
 //
 extern  t_shell *global_sh;
 
 //Prototypes
-void    main_loop(void);
-void    init_shell(t_shell *shell);
+void    init_shell(t_shell *shell, char	**env);
+char	**cpy_env(char **env);
 void	set_signals(void);
 void	sighandler(int signal);
+void    main_loop(t_shell *shell, char **env);
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include "../includes/minishell.h"
+#include <readline/readline.h>
 #include <signal.h>
+#include <unistd.h>
 
+/*global struct holding the state of my shell.*/
 t_shell	*global_sh;
 
-int main(int ac, char **av)
+/*
+	Read line.
+	Create history.
+	Handle Ctrl + c.
+	Call tokenizer.
+	Return exit value.
+*/
+
+int main(int ac, char **av, char  **env)
 {
 	t_shell	*shell;
 
@@ -12,18 +23,43 @@ int main(int ac, char **av)
     (void)ac;
     (void)av;
     shell = NULL;
-	shell = init_shell(shell);
+	shell = init_shell(shell, env);
 	global_sh = shell;
 	set_signals();
-
-
-    main_loop();
+	main_loop(shell, env);
 }
 
-void    init_shell(t_shell *shell)
+void    init_shell(t_shell *shell, char **env)
 {
+	shell = malloc(sizeof(t_shell));
     shell->input = NULL;
 	shell->tokens = NULL;
+	shell->exit_status = 0;
+	shell->env = cpy_env(env);
+}
+
+/*
+	Create a copy of env with allocation of memory.
+*/
+
+char	**cpy_env(char **env)
+{
+	size_t	i;
+
+	i = 0;
+	while(env[i])
+		i++;
+	char **new_env = (char **)malloc((i + 1) * sizeof(char *));
+	if (!new_env)
+		return NULL;
+	i = 0;
+	while (env[i])
+	{
+		new_env[i] = ft_strdup(env[i]);
+		i++;
+	}
+	new_env[i] = NULL;
+	return (new_env);
 }
 
 /*
@@ -46,12 +82,24 @@ void	set_signals(void)
 
 void	sighandler(int signal)
 {
-	
+	(void)signal;
+	if(global_sh)
+		global_sh->exit_status = 130;
+	rl_replace_line("", 0);
+	write(STDOUT_FILENO, "\n", 1);
+	rl_on_new_line();
+	rl_redisplay();
 }
 
+/*
+	Main interactive loop for minishell.
+	Handles the Read, Evaluate, Print Loop cycle for user interaction.
+		Reads input.
+		Process commands.
+		Manages exit conditions.
+*/
 
-
-void    main_loop(void)
+void    main_loop(t_shell *shell, char **env)
 {
     char *input;
 
